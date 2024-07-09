84c23bcac77adbda12307f28dc3cec6e0d39f218
mightyfoxer <azizamirul086@gmail.com>
2024-07-10 03:43:17 +0900

    initial commit

---
+++ resources/js/messenger.js
@@ -0,0 +1,16 @@
+/**
+ * -------------------------------------
+ * Reusable Functions
+ * -------------------------------------
+ */
+
+
+function imagePreview(input, selector) {
+    if ( input.files && input.files[0] ) {
+        var render = new FileReader();
+
+        render.onload = function(e) {
+
+        }
+    }
+}
